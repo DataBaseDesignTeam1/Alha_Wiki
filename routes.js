a3901fb2d9ab820310123a90553c46f6cf819a10
Yang,Dongwuk <donghae0414@naver.com>
2019-12-03 15:32:14 +0900

    install expressjs
    you should input command npm install

---
+++ routes.js
@@ -0,0 +1,22 @@
+
+const MAIN = "/";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SIGNUP = "/signup";
+
+
+const RECOMMEND = ":id/";
+
+const SMALL_CATEGORY = ":id/";
+
+const ALBA_DETAIL = ":id/";
+
+const POST_DETAIL = ":id/";
+
+const routes = {
+    main: MAIN,
+    login: LOGIN,
+    logout: LOGOUT,
+    signup: SIGNUP
+
+};
