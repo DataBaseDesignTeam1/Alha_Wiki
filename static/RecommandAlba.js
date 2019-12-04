007c7035add85a3d46e60129affa3f74c7693569
youngjae <dudwo1344@gmail.com>
2019-12-05 06:05:47 +0900

    add recommand myReview

--- static/RecommandAlba.js
+++ static/RecommandAlba.js
@@ -16,10 +16,17 @@ $(document).ready(function(){
         cache : false,
         processData: false,
         success: function (data) {
-            console.log(data);   
+            addMyReviewList(data);   
         },error:function(data){
             alert("error");
         }
     });
+    function addMyReviewList(data){
+        var MyReviewList = data.my_review_data;
+        console.log(MyReviewList);
+        for(var i = 0; i < MyReviewList.length ; i++){
+            $('.MyReviewList').append('<div class="MyReviewItem" value="'+MyReviewList[i].business_index+'"  style="float: none; color : #FFFFFF; font-weight:bold;"><div class="Number">'+(i+1)+'</div> <div class="BusinessName">'+MyReviewList[i].business_name+'</div><br> <div class="Content">'+MyReviewList[i].content+'</div><br> <div class="writeDate">'+MyReviewList[i].write_date+'</div> </div>');
+        }
+    }
 
 });
