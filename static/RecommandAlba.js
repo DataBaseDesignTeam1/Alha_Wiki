5f726ffb29a314ccffa9e55af6b9965a38c47d4e
youngjae <dudwo1344@gmail.com>
2019-12-05 07:03:19 +0900

    tmp

--- static/RecommandAlba.js
+++ static/RecommandAlba.js
@@ -27,21 +27,30 @@ $(document).ready(function(){
     function addMyReviewList(data){
         var MyReviewList = data.my_review_data;
         for(var i = 0; i < MyReviewList.length ; i++){
-            $('.MyReviewList').append('<div class="MyReviewItem" value="'+ MyReviewList[i].big_category_index +'/'+MyReviewList[i].small_category_index +'/'+ MyReviewList[i].business_index+ '"  style="float: none; color : #FFFFFF; font-weight:bold;"><div class="Number">'+(i+1)+'</div> <div class="businessState">'+MyReviewList[i].business_state+'</div><br><div class="businessCity">'+MyReviewList[i].business_city+'</div><div class="BusinessName">'+MyReviewList[i].business_name+'</div> <div class="writeDate">'+MyReviewList[i].write_date+'</div> </div>');
+            $('.MyReviewList').append('<div class="MyReviewItem" value="'+ MyReviewList[i].big_category_index +'/'+MyReviewList[i].small_category_index +'/'+ MyReviewList[i].business_index+ '"  style="float: none; color : #FFFFFF; font-weight:bold;"><div class="Number">'+(i+1)+'</div> <div class="businessState">'+MyReviewList[i].state+'</div><br><div class="businessCity">'+MyReviewList[i].city+'</div><div class="BusinessName" big ="'+MyReviewList[i].big_category_name+'" small="'+MyReviewList[i].small_category_name+'">'+MyReviewList[i].business_name+'</div> <div class="writeDate">'+MyReviewList[i].write_date+'</div> </div>');
         }
     }
     function addOhterReviwList(data){
         var OhterReviewList = data.recommand_data;
         for(var i = 0; i < OhterReviewList.length ; i++){
-            $('.OhterReviewList').append('<div class="OtherReviewItem" value="'+ OhterReviewList[i].big_category_index +'/'+OhterReviewList[i].small_category_index +'/'+ OhterReviewList[i].business_index+ '"  style="float: none; color : #FFFFFF; font-weight:bold;"><div class="Number">'+(i+1)+'</div> <div class="BusinessName">'+OhterReviewList[i].business_name+'</div><br> <div class="Content">'+OhterReviewList[i].content+'</div><br> <div class="writeDate">'+OhterReviewList[i].write_date+'</div> </div>');
+            $('.OhterReviewList').append('<div class="MyReviewItem" value="'+ OhterReviewList[i].big_category_index +'/'+OhterReviewList[i].small_category_index +'/'+ OhterReviewList[i].business_index+ '"  style="float: none; color : #FFFFFF; font-weight:bold;"><div class="Number">'+(i+1)+'</div> <div class="businessState">'+OhterReviewList[i].state+'</div><br><div class="businessCity">'+OhterReviewList[i].city+'</div><div class="BusinessName">'+OhterReviewList[i].business_name+'</div> <div class="writeDate">'+OhterReviewList[i].write_date+'</div> </div>');
         }
         console.log(OhterReviewList);
 
     }
       $(document).on("click",".MyReviewItem",function(){
+        var MyReviewItem = $(this).children();
+        var state = MyReviewItem[1].innerText;
+        var city = MyReviewItem[2].innerText;
+        var business = MyReviewItem[3].innerText;
+        var big_category = $(MyReviewItem[3]).attr('big');
+        var small_category =  $(MyReviewItem[3]).attr('small');
+
+        console.log(MyReviewItem[2].innerText);
+        // localStorage.setItem()
         var subURL = $(this).attr("value");
         var URL = "http://localhost:4000/category/" + subURL;
-        location.href = URL;
+        //location.href = URL;
     });
     $(document).on("click",".OtherReviewItem",function(){
         var subURL = $(this).attr("value");
