f95cd387b03772d315c379f81b7def3501511054
youngjae <dudwo1344@gmail.com>
2019-12-04 22:47:33 +0900

    modify login.js

--- static/login.js
+++ static/login.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('#LoginBtn').click(function(){
         var Id = $('#Id').val();
         localStorage.setItem("UserId",Id);
-        var URL = localStorage.getItem("URL");
-        //window.history.back();
+        var URL = localStorage.getItem("beforeLoginPage");
+        //window.history.back();    
         location.href = URL;
     });
     $('#SignUpBtn').click(function(){
